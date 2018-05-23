e9941ab6376afc126b69b3fc30155a0752a73304
Tim Lepage <timlepage9@gmail.com>
2018-05-23 13:09:56 -0300

    second project started and first one cleaned up

--- GroundTracking/Assets/CloseButton.cs
+++ GroundTracking/Assets/CloseButton.cs
@@ -30,13 +30,12 @@ public class CloseButton : MonoBehaviour {
     {
         //Gets the position of the first flag
         coord_new = pf.GetComponent<SavePositions>().getPosList(1);
-        coord_old = getVector3(pos.text);
+        coord_old = flag.GetComponent<DistanceMeasurement>().getVector3(pos.text);
         GameObject instanciated_ruler = GameObject.Instantiate(ruler);
         //Calculates the distance between the two last points and displays it in the field "dist" and "dist3D"
         d = (float)Math.Sqrt(Math.Pow(coord_new.x - coord_old.x, 2) + Math.Pow(coord_new.z - coord_old.z, 2)) * 100;
         dist.text = d.ToString("G4") + " cm";
         instanciated_ruler.GetComponentInChildren<TextMesh>().text = d.ToString("G4") + " cm";
-        //pos.text = "0.0, 0.0, 0.0";
         pos.text = coord_new.ToString("G4");
         //places the object "Ruler" in between the two last points
         instanciated_ruler.transform.position = new Vector3((coord_old.x + coord_new.x) / 2, (coord_old.y + coord_new.y) / 2, (coord_old.z + coord_new.z) / 2);
@@ -44,29 +43,13 @@ public class CloseButton : MonoBehaviour {
         //Gets the angle between the vector created with the two last points and the x axis
         if (coord_new.z > coord_old.z)
         {
-            angle = 360 - GetAngle_x(new Vector3(coord_new.x - coord_old.x, coord_new.y - coord_old.y, coord_new.z - coord_old.z));
+            angle = 360 - flag.GetComponent<DistanceMeasurement>().GetAngle_x(new Vector3(coord_new.x - coord_old.x, coord_new.y - coord_old.y, coord_new.z - coord_old.z));
         }
         else
         {
-            angle = GetAngle_x(new Vector3(coord_new.x - coord_old.x, coord_new.y - coord_old.y, coord_new.z - coord_old.z));
+            angle = flag.GetComponent<DistanceMeasurement>().GetAngle_x(new Vector3(coord_new.x - coord_old.x, coord_new.y - coord_old.y, coord_new.z - coord_old.z));
         }
         instanciated_ruler.transform.rotation = Quaternion.Euler(0f, angle, 0f);
         
-    }
-
-    public float GetAngle_x(Vector3 ab)
-    {
-        return (float)Math.Acos(ab.x / Math.Sqrt(ab.x * ab.x + ab.y * ab.y + ab.z * ab.z)) * 180 / (float)Math.PI;
-    }
-
-    //This function creates a Vector3 from a string
-    public Vector3 getVector3(string rString)
-    {
-        string[] temp = rString.Substring(1, rString.Length - 2).Split(',');
-        float x = float.Parse(temp[0]);
-        float y = float.Parse(temp[1]);
-        float z = float.Parse(temp[2]);
-        Vector3 rValue = new Vector3(x, y, z);
-        return rValue;
     }
 }
